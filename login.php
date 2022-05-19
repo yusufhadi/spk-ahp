5fb3c65d5f037926f5da665b19601ed1b6f0de9f
yusufhadi <hadiyusuf889@gmail.com>
2022-05-19 22:34:52 +0800

    slicing page login

--- login.php
+++ login.php
@@ -3,31 +3,57 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>LOGIN</title>
-  <link href="assets/css/sandstone-bootstrap.min.css" rel="stylesheet" />
-  <link href="assets/css/signin.css" rel="stylesheet" />
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login</title>
+
+  <!-- Font Icon -->
+  <link rel="stylesheet" href="assets/fonts/material-icon/css/material-design-iconic-font.min.css">
+
+  <!-- Main css -->
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
-  <div class="container">
-    <form class="form-signin" action="?act=login" method="post">
-      <h2 class="form-signin-heading" style="color: black;">Silahkan masuk</h2>
-      <?php if ($_POST) include 'aksi.php'; ?>
-      <label for="inputEmail" class="sr-only">Usernames</label>
-      <input type="text" id="inputEmail" class="form-control" placeholder="Username" name="user" autofocus />
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="pass" />
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Masuk</button>
-    </form>
+
+  <div class="main">
+
+    <!-- Sing in  Form -->
+    <section class="sign-in">
+      <div class="container">
+        <div class="signin-content">
+          <div class="signin-image">
+            <figure><img src="assets/images/signin-image.jpg" alt="sing up image"></figure>
+          </div>
+          <div class="signin-form">
+            <h2 class="form-title">Sign up</h2>
+            <form method="POST" class="register-form" id="login-form" action="?act=login" method="post">
+              <?php if ($_POST) include 'aksi.php'; ?>
+              <div class="form-group">
+                <label for="inputEmail"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                <input type="text" name="user" id="inputEmail" placeholder="Username" />
+              </div>
+              <div class="form-group">
+                <label for="inputPassword"><i class="zmdi zmdi-lock"></i></label>
+                <input type="password" name="pass" id="inputPassword" placeholder="Password" />
+              </div>
+              <div class="form-group form-button">
+                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </div>
   <script type="text/javascript">
     $('.form-control').attr('autocomplete', 'off');
   </script>
-</body>
+  <!-- JS -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
 </html>
